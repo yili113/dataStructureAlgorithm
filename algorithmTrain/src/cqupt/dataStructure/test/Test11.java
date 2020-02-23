51c6c57752a955ffe4b8c81ca8f01ba1536bf8be
yili113 <734236009@qq.com>
2020-02-23 16:34:31 +0800

    no message

---
+++ algorithmTrain/src/cqupt/dataStructure/test/Test11.java
@@ -0,0 +1,11 @@
+package cqupt.dataStructure.test;
+
+/**
+ * @author Liyi
+ * @create 2020-02-23 16:33
+ */
+public class Test11 {
+    public static void main(String[] args) {
+        System.out.println("sss");
+    }
+}
