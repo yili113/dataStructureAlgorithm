4bb865c677e5633dcbab3b56f911593ee25d3b20
李燚 <734236009@qq.com>
2020-03-03 22:59:31 +0800

    no message

--- algorithmTrain/src/cqupt/leetCode/T21.java
+++ algorithmTrain/src/cqupt/leetCode/T21.java
@@ -14,8 +14,8 @@ public class T21 {
         ListNode cur2 = l2;
         while (cur1 != null && cur2 != null) {
             if (cur1.val <= cur2.val) {
-                cur.next = cur1;
-                cur = cur.next;
+                cur.next = cur1;// 我自己写的是 new一个结点存放cur1的val 接在cur后面
+                cur = cur.next;// 其实没必要 有cur1不断指向  链表1是不会丢的
                 cur1 = cur1.next;
             }else {
                 cur.next = cur2;
