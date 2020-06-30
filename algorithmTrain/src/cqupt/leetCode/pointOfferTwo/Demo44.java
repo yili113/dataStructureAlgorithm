f1506623abb0773a31d87f7504cfcd1d0df78587
yili113 <734236009@qq.com>
2020-06-30 19:00:46 +0800

    6.30

---
+++ algorithmTrain/src/cqupt/leetCode/pointOfferTwo/Demo44.java
@@ -0,0 +1,11 @@
+package cqupt.leetCode.pointOfferTwo;
+
+/**
+ * @author yiLi
+ * @create 2020-06-30 8:35
+ */
+public class Demo44 {
+    public int findNthDigit(int n) {
+        return 0;
+    }
+}
