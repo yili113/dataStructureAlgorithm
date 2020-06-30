547bc712ffca3b0090809b9585166e0784270f15
yili113 <734236009@qq.com>
2020-06-30 20:00:32 +0800

    6.30

--- algorithmTrain/src/cqupt/leetCode/pointOfferTwo/Demo44.java
+++ algorithmTrain/src/cqupt/leetCode/pointOfferTwo/Demo44.java
@@ -6,6 +6,20 @@ package cqupt.leetCode.pointOfferTwo;
  */
 public class Demo44 {
     public int findNthDigit(int n) {
-        return 0;
+        // 1.找到n是属于几位数构成的数位中的
+        int digit = 1;
+        long start = 1;// 在大数运算中定义成int会出错
+        long count = 9;// 初始化先减由一位数构成的数位
+        while (n > count) {
+            n -= count;
+            start *= 10;
+            digit += 1;
+            count = 9 * digit * start;
+        }
+        // 2.找到digit位数中的具体哪一个数
+        long num = start + (n - 1) / digit;// 这个位数中每个数都占了digit个数位
+        // 3.找到在num这个数中具体是哪一个数位
+        // 此时剩下的数位个数n肯定是小于digit的
+        return Long.toString(num).charAt((n - 1) % digit) - '0';
     }
 }
---
+++ algorithmTrain/src/cqupt/leetCode/pointOfferTwo/sort/InsertSort.java
@@ -0,0 +1,51 @@
+package cqupt.leetCode.pointOfferTwo.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author yiLi
+ * @create 2020-06-30 19:01
+ */
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] arr = {8, 7, 5, 2, 3, 1, 2};
+        System.out.println(Arrays.toString(sort(arr)));
+    }
+
+    private static int[] sort(int[] nums) {
+//        // 第一轮
+//        // 把第一个位置的元素插入到前面的位置,初始要插入的位置就是前一位置
+//        int insertVal = nums[1];
+//        int insertIndex = 0;
+//        while (insertIndex >= 0 && nums[insertIndex] > insertVal) {
+//            nums[insertIndex + 1] = nums[insertIndex];
+//            insertIndex --;
+//        }
+//        nums[insertIndex + 1] = insertVal;
+//
+//        // 第二轮
+//        insertVal = nums[2];
+//        insertIndex = 1;
+//        while (insertIndex >= 0 && nums[insertIndex] > insertVal) {
+//            nums[insertIndex + 1] = nums[insertIndex];
+//            insertIndex --;
+//        }
+//        nums[insertIndex + 1] = insertVal;
+
+        // 合并起
+        int insertIndex = 0;
+        int insertVal = 0;
+        for (int i = 1; i < nums.length; i++) {
+            insertVal = nums[i];
+            insertIndex = i - 1;
+            while (insertIndex >= 0 && nums[insertIndex] > insertVal) {
+                nums[insertIndex + 1] = nums[insertIndex];
+                insertIndex --;
+            }
+            if ((insertIndex + 1) != (i - 1))// 进行优化
+                nums[insertIndex + 1] = insertVal;
+//            nums[insertIndex + 1] = insertVal;
+        }
+        return nums;
+    }
+}
