27dc2175f20ce42f562bc4c8ea9af6eae06e021f
yili113 <734236009@qq.com>
2020-07-05 09:16:26 +0800

    7.04

--- algorithmTrain/src/cqupt/leetCode/pointOfferTwo/Demo58_1.java
+++ algorithmTrain/src/cqupt/leetCode/pointOfferTwo/Demo58_1.java
@@ -1,8 +1,27 @@
 package cqupt.leetCode.pointOfferTwo;
 
+import java.util.ArrayList;
+
 /**
  * @author yiLi
  * @create 2020-07-04 15:15
+ * 翻转单词顺序
  */
 public class Demo58_1 {
+    public static String reverseWords(String s) {
+        String str = s.trim();
+        String[] strs = s.split("\\s+");
+        ArrayList<String> list = new ArrayList<>();
+        for (String ss : strs)
+            list.add(ss);
+        StringBuilder sb = new StringBuilder();
+        for (int i = list.size() - 1; i >= 0; i--) {
+            sb.append(list.get(i)).append(" ");
+        }
+        return sb.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseWords("the sky is blue"));
+    }
 }
--- algorithmTrain/src/cqupt/leetCode/pointOfferTwo/Demo60.java
+++ algorithmTrain/src/cqupt/leetCode/pointOfferTwo/Demo60.java
@@ -3,6 +3,26 @@ package cqupt.leetCode.pointOfferTwo;
 /**
  * @author yiLi
  * @create 2020-07-04 15:29
+ * n个骰子的点数
  */
 public class Demo60 {
+    public double[] twoSum(int n) {
+        int[][] dp = new int[n + 1][6 * n + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 6 * n; j++) {// i个骰子一共能得到的点数
+                for (int k = 1; k <= Math.min(6, j); k++) {// 第i个骰子可以贡献的点数
+                    dp[i][j] += dp[i - 1][j - k];
+                }
+            }
+        }
+        int totalCount = 0;
+        for (int num : dp[n])
+            totalCount += num;
+        double[] res = new double[6 * n - (n - 1)];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = (double) dp[n][n + i] / totalCount;
+        }
+        return res;
+    }
 }
